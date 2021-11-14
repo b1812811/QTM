2f015a970af661e8a9cfa77efe5dd63cfe4e0cdc
Duy Quang <83594819+b1812811@users.noreply.github.com>
2021-11-14 20:24:28 +0700

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = "mongodb+srv://b1812811:<your_password>@cluster0.nkmt6.mongodb.net/EmployeeDB";
+const url = "mongodb+srv://b1812811:<your_password>@*********.mongodb.net/EmployeeDB";
 
 mongoose.connect(url,{useNewUrlParser:true},(err) => {
     if(!err){ console.log("MongoDB Connection Succeeded");}
